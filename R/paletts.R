c44e1167f1dd684b9bb02158a917e2e7d30e4f4a
Michaeladebolt <32584911+Michaeladebolt@users.noreply.github.com>
2022-01-27 15:47:13 -0800

    fixing typos
    fixing typos

--- R/paletts.R
+++ R/paletts.R
@@ -1,6 +1,6 @@
 ## color pallets based on eye shadow pallets
 
-colouRpop <- function(pallet = "lust_for_dark", show_me = FALSE){
+colouRpop <- function(palett = "lust_for_dark", show_me = FALSE){
   library(ggplot2)
   library(cowplot)
 
@@ -57,51 +57,43 @@ colouRpop <- function(pallet = "lust_for_dark", show_me = FALSE){
 
 
   # Combine all the pallets in a list
-  all_pallets <- list(lust_for_dark, limoncello, so_very_lovely,
+  all_paletts <- list(lust_for_dark, limoncello, so_very_lovely,
                       big_poppy, blowin_smoke, youre_golden,
                       the_child, malibu_barbie, baroque, its_a_mood, its_a_princess_thing)
 
   # assign the pallets depending on what the user wants
-  if ( pallet == "lust_for_dark") {
-    pallet_choice <- all_pallets[[1]]
-  } else if ( pallet == "limoncello") {
-    pallet_choice <- all_pallets[[2]]
-  }  else if (pallet == "so_very_lovely") {
-    pallet_choice <- all_pallets[[3]]
-  } else if (pallet == "big_poppy") {
-    pallet_choice <- all_pallets[[4]]
-  } else if (pallet == "blowin_smoke") {
-    pallet_choice <- all_pallets[[5]]
-  } else if (pallet == "youre_golden") {
-    pallet_choice <- all_pallets[[6]]
-  } else if (pallet == "the_child") {
-    pallet_choice <- all_pallets[[7]]
-  } else if (pallet == "malibu_barbie") {
-    pallet_choice <- all_pallets[[8]]
-  } else if (pallet == "baroque") {
-    pallet_choice <- all_pallets[[9]]
-  } else if (pallet == "its_a_mood") {
-    pallet_choice <- all_pallets[[10]]
+  if ( palett == "lust_for_dark") {
+    palett_choice <- all_paletts[[1]]
+  } else if ( palett == "limoncello") {
+    palett_choice <- all_paletts[[2]]
+  }  else if (palett == "so_very_lovely") {
+    palett_choice <- all_paletts[[3]]
+  } else if (palett == "big_poppy") {
+    palett_choice <- all_paletts[[4]]
+  } else if (palett == "blowin_smoke") {
+    palett_choice <- all_paletts[[5]]
+  } else if (palett == "youre_golden") {
+    palett_choice <- all_paletts[[6]]
+  } else if (palett == "the_child") {
+    palett_choice <- all_paletts[[7]]
+  } else if (palett == "malibu_barbie") {
+    palett_choice <- all_paletts[[8]]
+  } else if (palett == "baroque") {
+    palett_choice <- all_paletts[[9]]
+  } else if (palett == "its_a_mood") {
+    palett_choice <- all_paletts[[10]]
   } else {
-    pallet_choice <- all_pallets[[11]] #its a princess thing
+    palett_choice <- all_paletts[[11]] #its a princess thing
   }
 
   if(show_me == TRUE){
-    # Create demo plot of the colors to print - would be fun to print them
-    # and make them look like an eyeshadow pallet!
-    # cute_plot <- ggplot(data.frame(x = seq(1:length(pallet_choice)),
-    #                                y = seq(1:length(pallet_choice)) ),
-    #                     aes(x = x, y = y)) +
-    #   geom_point(color = pallet_choice, size = 12) +
-    #   theme_void() +
-    #   ggtitle(pallet)
-    cute_plot <- ggplot(data = data.frame(name = as.factor(seq(1:length(pallet_choice))),
-                                          value = rep(length(pallet_choice))),
+    cute_plot <- ggplot(data = data.frame(name = as.factor(seq(1:length(palett_choice))),
+                                          value = rep(length(palett_choice))),
                         aes(x = name, y = value)) +
       geom_bar(stat = "identity",
-               fill = pallet_choice) +
+               fill = palett_choice) +
       theme_void() +
-      ggtitle(pallet)
+      ggtitle(palett)
     # img_plt <- ggdraw() + draw_image("big_poppy.png")
     # #Combine picture and demo plot
     # plot_grid(cute_plot, img_plt, nrow = 1)
@@ -113,7 +105,7 @@ colouRpop <- function(pallet = "lust_for_dark", show_me = FALSE){
   if(show_me == TRUE){
     return(cute_plot)
   }else{
-    return(pallet_choice)
+    return(palett_choice)
   }
 }
 
